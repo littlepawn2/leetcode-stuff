340687fc2c01c1ecd1ddd14475c341c6056c606f
William Wen <littlepawn2@gmail.com>
2021-05-10 10:41:16 -0400

    Update RomanToInteger.java

--- Java/LeetCode/src/RomanToInteger.java
+++ Java/LeetCode/src/RomanToInteger.java
@@ -2,8 +2,66 @@
 public class RomanToInteger {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(romanToInt("IV"));
 
 	}
+	
+    public static int romanToInt(String s) {
+        char last = ' ';
+        int num = 0;
+        
+        for(int i=0; i<s.length(); i++) {
+        	switch(s.charAt(i)) {
+        	case 'M':
+        		if(last == 'C') {
+        			num += 800;
+        		} else {
+        			num += 1000;
+        		}
+        		break;
+        	case 'D':
+        		if(last == 'C') {
+        			num += 300;
+        		} else {
+        			num += 500;
+        		}
+        		break;
+        	case 'C':
+        		if(last == 'X') {
+        			num += 80;
+        		} else {
+        			num += 100;
+        		}
+        		break;
+        	case 'L':
+        		if(last == 'X') {
+        			num += 30;
+        		} else {
+        			num += 50;
+        		}
+        		break;
+        	case 'X':
+        		if(last == 'I') {
+        			num += 8;
+        		} else {
+        			num += 10;
+        		}
+        		break;
+        	case 'V':
+        		if(last == 'I') {
+        			num += 3;
+        		} else {
+        			num += 5;
+        		}
+        		break;
+        	case 'I':
+        		num += 1;
+        		break;
+        	}
+        	last = s.charAt(i);
+        }
+        
+        return num;
+    }
 
 }
