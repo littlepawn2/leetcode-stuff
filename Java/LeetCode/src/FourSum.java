36bc3913382423520be95040abcec68f0c1f27ef
William Wen <littlepawn2@gmail.com>
2021-05-12 22:39:48 -0400

    Create FourSum.java

---
+++ Java/LeetCode/src/FourSum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class FourSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> quads = new ArrayList<>();
+        
+        /* for each number run three sum */
+        
+        return null;
+    }
+
+}
